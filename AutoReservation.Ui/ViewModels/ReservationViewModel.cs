4ed41fe7dbe255748f2f63c7473b5c322fa51b81
greekins <marco.leutenegger@outlook.com>
2014-12-09 13:27:34 +0100

    View

---
+++ AutoReservation.Ui/ViewModels/ReservationViewModel.cs
@@ -0,0 +1,234 @@
+﻿using AutoReservation.Common.DataTransferObjects;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AutoReservation.Ui.ViewModels
+{
+    class ReservationViewModel : ViewModelBase
+    {
+        private readonly List<ReservationDto> reservationenOriginal = new List<ReservationDto>();
+        private ObservableCollection<ReservationDto> reservationen;
+        public ObservableCollection<ReservationDto> Reservationen
+        {
+            get
+            {
+                if (reservationen == null)
+                {
+                    reservationen = new ObservableCollection<ReservationDto>();
+                }
+                return reservationen;
+            }
+        }
+
+        private ObservableCollection<AutoDto> autos;
+        public ObservableCollection<AutoDto> Autos
+        {
+            get
+            {
+                if (autos == null)
+                {
+                    autos = new ObservableCollection<AutoDto>();
+                }
+                return autos;
+            }
+        }
+
+        private ObservableCollection<KundeDto> kunden;
+        public ObservableCollection<KundeDto> Kunden
+        {
+            get
+            {
+                if (kunden == null)
+                {
+                    kunden = new ObservableCollection<KundeDto>();
+                }
+                return kunden;
+            }
+        }
+
+        private ReservationDto selectedReservation;
+        public ReservationDto SelectedReservation
+        {
+            get { return selectedReservation; }
+            set
+            {
+                if (selectedReservation != value)
+                {
+                    selectedReservation = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+
+        #region Load-Command
+
+        private RelayCommand loadCommand;
+
+        public ICommand LoadCommand
+        {
+            get
+            {
+                if (loadCommand == null)
+                {
+                    loadCommand = new RelayCommand(
+                        param => Load(),
+                        param => CanLoad()
+                    );
+                }
+                return loadCommand;
+            }
+        }
+
+        protected override void Load()
+        {
+            Reservationen.Clear();
+            reservationenOriginal.Clear();
+            foreach (ReservationDto reservation in Service.Reservationen)
+            {
+                Reservationen.Add(reservation);
+                reservationenOriginal.Add((ReservationDto)reservation.Clone());
+            }
+            SelectedReservation = Reservationen.FirstOrDefault();
+        }
+
+        private bool CanLoad()
+        {
+            return Service != null;
+        }
+
+        #endregion
+
+        #region Save-Command
+
+        private RelayCommand saveCommand;
+
+        public ICommand SaveCommand
+        {
+            get
+            {
+                if (saveCommand == null)
+                {
+                    saveCommand = new RelayCommand(
+                        param => SaveData(),
+                        param => CanSaveData()
+                    );
+                }
+                return saveCommand;
+            }
+        }
+
+        private void SaveData()
+        {
+            foreach (ReservationDto reservation in Reservationen)
+            {
+                if (reservation.ReservationNr == default(int))
+                {
+                    Service.InsertReservation(reservation);
+                }
+                else
+                {
+                    ReservationDto original = reservationenOriginal.FirstOrDefault(ao => ao.ReservationNr == reservation.ReservationNr);
+                    Service.UpdateReservation(reservation, original);
+                }
+            }
+            Load();
+        }
+
+        private bool CanSaveData()
+        {
+            if (Service == null)
+            {
+                return false;
+            }
+
+            StringBuilder errorText = new StringBuilder();
+            foreach (ReservationDto reservation in Reservationen)
+            {
+                string error = reservation.Validate();
+                if (!string.IsNullOrEmpty(error))
+                {
+                    errorText.AppendLine(reservation.ToString());
+                    errorText.AppendLine(error);
+                }
+            }
+
+            ErrorText = errorText.ToString();
+            return string.IsNullOrEmpty(ErrorText);
+        }
+
+
+        #endregion
+
+        #region New-Command
+
+        private RelayCommand newCommand;
+
+        public ICommand NewCommand
+        {
+            get
+            {
+                if (newCommand == null)
+                {
+                    newCommand = new RelayCommand(
+                        param => New(),
+                        param => CanNew()
+                    );
+                }
+                return newCommand;
+            }
+        }
+
+        private void New()
+        {
+            Reservationen.Add(new ReservationDto());
+        }
+
+        private bool CanNew()
+        {
+            return Service != null;
+        }
+
+        #endregion
+
+        #region Delete-Command
+
+        private RelayCommand deleteCommand;
+
+        public ICommand DeleteCommand
+        {
+            get
+            {
+                if (deleteCommand == null)
+                {
+                    deleteCommand = new RelayCommand(
+                        param => Delete(),
+                        param => CanDelete()
+                    );
+                }
+                return deleteCommand;
+            }
+        }
+
+        private void Delete()
+        {
+            Service.DeleteReservation(SelectedReservation);
+            Load();
+        }
+
+        private bool CanDelete()
+        {
+            return
+                SelectedReservation != null &&
+                SelectedReservation.ReservationNr != default(int) &&
+                Service != null;
+        }
+
+        #endregion
+    }
+}
---
+++ AutoReservation.Ui/Views/ReservationView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace AutoReservation.Ui.Views
+{
+    /// <summary>
+    /// Interaktionslogik für ReservationView.xaml
+    /// </summary>
+    public partial class ReservationView : UserControl
+    {
+        public ReservationView()
+        {
+            InitializeComponent();
+        }
+    }
+}
