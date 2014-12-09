7675a3b7602da0e9106bcd83e623aa421cb87d1b
Dominik Freier <dfreier@hsr.ch>
2014-12-09 14:30:55 +0100

    test ui

--- AutoReservation.Ui.Testing/ViewModelTest.cs
+++ AutoReservation.Ui.Testing/ViewModelTest.cs
@@ -1,35 +1,48 @@
 ﻿using AutoReservation.TestEnvironment;
 using AutoReservation.Ui.ViewModels;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Windows.Input;
 
 namespace AutoReservation.Ui.Testing
 {
     [TestClass]
     public class ViewModelTest
     {
+        private ReservationViewModel reservationViewModel;
+        private KundeViewModel kundeViewModel;
+        private AutoViewModel autoViewModel;
+
+
         [TestInitialize]
         public void InitializeTestData()
         {
             TestEnvironmentHelper.InitializeTestData();
+            autoViewModel = new AutoViewModel();
+            kundeViewModel = new KundeViewModel();
+            reservationViewModel = new ReservationViewModel();
         }
 
         [TestMethod]
         public void AutosLoadTest()
         {
-            Assert.Inconclusive("Test wurde noch nicht implementiert!");
+            Assert.IsTrue(autoViewModel.LoadCommand.CanExecute(null));
+            autoViewModel.LoadCommand.Execute(null);
+            Assert.AreEqual(3, autoViewModel.Autos.Count);
         }
 
         [TestMethod]
         public void KundenLoadTest()
         {
-            Assert.Inconclusive("Test wurde noch nicht implementiert!");
+            Assert.IsTrue(kundeViewModel.LoadCommand.CanExecute(null));
+            autoViewModel.LoadCommand.Execute(null);
+            Assert.AreEqual(4, kundeViewModel.Kunden.Count);
         }
 
         [TestMethod]
         public void ReservationenLoadTest()
         {
-            Assert.Inconclusive("Test wurde noch nicht implementiert!");
+            Assert.IsTrue(reservationViewModel.LoadCommand.CanExecute(null));
+            autoViewModel.LoadCommand.Execute(null);
+            Assert.AreEqual(3, reservationViewModel.Reservationen.Count);
         }
     }
 }
